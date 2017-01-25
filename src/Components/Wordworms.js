1721942ee46220568d4ad1b362fc2079c3a9fd25
Casey McCaskill <caseymccaskill@Caseys-MacBook-Pro.local>
2017-01-25 16:35:06 -0500

    init commit

---
+++ src/Components/Wordworms.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Wordworms extends Component{
+	render(){
+		return(
+			<div>
+			You are at Wordworms
+			</div>
+		);
+	}
+}
+
+export default Wordworms
